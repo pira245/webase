10091849dfcc5a595ad48c9e23084d150c83d7a0
pira.245.az <ricardo.pirabed@egmail.com>
2025-01-30 19:34:25 +0100

    functional webase migrate + testing

---
+++ core/mytestunity/pytest_basics/run_test_unit_tests_functions.py
@@ -0,0 +1,17 @@
+import pytest
+##############################################################################################
+"""
+    Testing methods and classes for functions unit tests
+"""
+##############################################################################################
+# Import all test files
+import core
+import core.mytestunity.system_file
+import core.web_core
+##############################################################################################
+
+
+
+if __name__ == "__main__":
+    # Run all test available
+	print("Test")
